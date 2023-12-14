30a0b6d0ad064bba292232ed3a3fe22d43e6a2f2
foyinjohn <fasasifoyin@gmail.com>
2023-12-14 09:33:53 -1000

    third commit

--- dist/app.js
+++ dist/app.js
@@ -40,7 +40,7 @@ const connect_mongo_1 = __importDefault(require("connect-mongo"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mercebackend.onrender.com"],
     credentials: true,
 }));
 app.use((0, express_session_1.default)({
